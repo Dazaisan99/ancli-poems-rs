bf62915a295bbfce53f66d5cf71848ec1455df0c
Dazaisan99 <smugdazai6d@gmail.com>
2022-08-24 17:14:43 +0200

    Bits and bobs, changed the readme

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-todo
+#Ancli-poems
+
+Ancli-poems is a simple command line tool to help create anki cards from poems.
+
+##<details><summary>Installation</summary>
+<p>
+
+Git clone the repository
+
+
+```
+$ cargo install ancli
+```
+
+</p>
+</details>
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use ancli_poetry::*;
-use clap::*;
+use ancli_poems as a;
+use clap::{Command, Arg};
 use std::path::Path;
 
 fn main() {
@@ -30,5 +30,5 @@ fn main() {
 
     let path = Path::new(&path_arg);
 
-    create_deck(get_contents(&path), outpath).unwrap();
+    a::create_deck(a::get_contents(&path), outpath).unwrap();
 }
